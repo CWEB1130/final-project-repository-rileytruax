21d81fe8777bc78362642f22aff29cddc84d9347
Jackson Band <banjacl@dunwoody.edu>
2021-05-21 21:16:22 -0500

    layouts

--- routes/api.js
+++ routes/api.js
@@ -6,14 +6,14 @@ router.get('/', function(req, res, next) {
         uri: 'http://www.omdbapi.com/?i=tt3896198&apikay=5c9a0845',
         qs: {
             api_key: '5c9a0845',
-            query: 'The Dark Night Rises'
+            query: 'The Dark Knight Rises'
         },
         function(error, response, body) {
             if(!error && response.statusCode === 200) {
                 console.log(body)
             }
         }
-    });
+    }).pipe(res);
 });
 
 module.exports = router;
